e4034c55df895661ab7db909e9c02024257d5155
Satish Kumar Patra <44961824+sitasp@users.noreply.github.com>
2025-02-22 13:11:52 +0530

    Time: 46 ms (83.26%) | Memory: 55.2 MB (63.34%) - LeetSync

--- LeetCode/940-fruit-into-baskets/fruit-into-baskets.java
+++ LeetCode/940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,65 +1,27 @@
 class Solution {
 
-    class Pair <R, S> {
-        R first;
-        S second;
-
-        Pair(R first, S second){
-            this.first = first;
-            this.second = second;
-        }
-    }
-
-
-    class LRUCache <K, V> extends LinkedHashMap<K, V> {
-
-        final int capacity;
-        Stack<Pair<Map.Entry<K, V>, Boolean>> stack;
-
-        LRUCache(int capacity) {
-            super(capacity, 0.75f, true);
-            this.capacity = capacity;
-            this.stack = new Stack<>();
-        }
-
-        // called after put method
-        @Override
-        protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
-            boolean shouldEvict = size() > capacity;
-            if(shouldEvict){
-                stack.add(new Pair<>(eldest, false));
-            }
-            return shouldEvict;
-        }
-
-        public Map.Entry<K, V> getEvictedValue(){
-            while(!stack.empty() && stack.peek().second){
-                stack.pop();
-            }
-            if(stack.isEmpty()){
-                return null;
-            }
-            Pair<Map.Entry<K, V>, Boolean> topElem = stack.peek();
-            topElem.second = true;
-            return topElem.first;
-        }
-    }
-
     public int totalFruit(int[] fruits) {
         
         int l = 0, r = 0;
         
-        LRUCache<Integer, Integer> lru = new LRUCache<>(2);
+        Map<Integer, Integer> map = new HashMap<>();
 
         int cnt = 0;
         int ans = 0;
 
         for(;r<fruits.length;r++){
-            lru.put(fruits[r], r);
-            Map.Entry<Integer, Integer> evictedElem = lru.getEvictedValue();
-            if(Objects.nonNull(evictedElem)){
-                int pos = evictedElem.getValue();
-                l = pos + 1;
+            if(map.containsKey(fruits[r])){
+                map.put(fruits[r], map.get(fruits[r]) + 1);
+            }
+            else map.put(fruits[r], 1);
+            while(map.size() > 2 && l < r){
+                int leftFruitTypeCount = map.get(fruits[l]);
+                leftFruitTypeCount--;
+                if(leftFruitTypeCount == 0){
+                    map.remove(fruits[l]);
+                }
+                else map.put(fruits[l], leftFruitTypeCount);
+                l++;
             }
             ans = Math.max(ans, r - l + 1);
         }
