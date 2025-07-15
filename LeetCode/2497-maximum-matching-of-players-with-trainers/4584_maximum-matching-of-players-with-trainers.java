9641a20c428e48fb474083dc0211fa91bef3cc4c
Satish Kumar Patra <44961824+sitasp@users.noreply.github.com>
2025-07-15 21:27:04 +0530

    Time: 26 ms (61.53%) | Memory: 57.3 MB (98.19%) - LeetSync

---
+++ LeetCode/2497-maximum-matching-of-players-with-trainers/4584_maximum-matching-of-players-with-trainers.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+
+        int i = 0, j = 0;
+        int matches = 0;
+        while(i < players.length && j < trainers.length) {
+            if(players[i] <= trainers[j]){
+                i++;
+                matches++;
+            }
+            j++;
+        }
+
+        return matches;
+    }
+}
